f42bcff89984cb7bf46fddc72345d4f93a02e25a
lahl82 <luisadolfo.hernandezlara@gmail.com>
2022-11-05 11:02:22 -0400

    Agregado show a ContractsController que retorna horas requeridad para un id de contract

--- app/controllers/api/v1/contracts_controller.rb
+++ app/controllers/api/v1/contracts_controller.rb
@@ -4,9 +4,25 @@ module Api
   module V1
     # clase Contratos de Servicios de MaaS
     class ContractsController < ApplicationController
+      before_action :set_contract, only: %i[ show edit update destroy ]
+
       def index
         @contracts = Contract.where(status: true)
         render json: @contracts
+      end
+
+      def show 
+        response = @contract.requirement
+        render json: response
+      end
+
+      private
+      def set_contract
+        @contract = Contract.find(params[:id])
+      end
+  
+      def contract_params
+        #params.require(:contract).permit(:title, :content, :state)
       end
     end
   end
